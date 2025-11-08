23e74bc0ea2cde3d4ed93272abbd486945399f14
demid <i.demidov@innopolis.university>
2025-11-09 00:47:12 +0300

    cosine similarity approach

--- libs/utils.py
+++ libs/utils.py
@@ -50,3 +50,47 @@ def DCG(submission: pd.DataFrame, true_reactions: pd.DataFrame):
 
     dcg = df.apply(calculate_dcg, axis=1).mean()
     return dcg
+
+
+def unconstrained_top_selection(df: pd.DataFrame, top_per_item: int=100) -> pd.DataFrame:
+    """:param df index is USER, columns are ITEMs, entries are scores
+    :param top_per_item maximum amount of USERs selected for each ITEM
+    :returns DataFrame with columns (ITEM, USER), where USER is List[top_per_item]"""
+    return df \
+        .apply(lambda col: col.nlargest(top_per_item).index.tolist(), axis=0) \
+        .melt(var_name=ITEM, value_name=USER) \
+        .dropna() \
+        .groupby(ITEM)[USER] \
+        .apply(list) \
+        .reset_index()
+
+
+def constrained_top_selections(df: pd.DataFrame, top_per_item: int=100, max_per_user: int=101) -> pd.DataFrame:
+    """:param df index is USER, columns are ITEMs, entries are scores
+    :param top_per_item maximum amount of USERs selected for each ITEM
+    :param max_per_user maximum amount of selections per each user
+    :returns DataFrame with columns (ITEM, USER), where USER is List[top_per_item]"""
+    user_counts = defaultdict(int)
+    results = []
+
+    for item in df.columns:
+        item_scores = []
+        for user in df.index:
+            score = df.loc[user, item]
+            item_scores.append((-score, user))
+
+        item_scores.sort()
+
+        selected_for_item = []
+        for neg_score, user in item_scores:
+            if len(selected_for_item) >= top_per_item:
+                break
+            if user_counts[user] < max_per_user:
+                selected_for_item.append(user)
+                user_counts[user] += 1
+                results.append({
+                    USER: user,
+                    ITEM: item,
+                })
+
+    return pd.DataFrame(results).groupby(ITEM).agg({USER: list})
